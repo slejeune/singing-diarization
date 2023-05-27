4d05b11923429fe30b9a8bf7b2389dfb858242af
Suzan Lejeune <suzan.lejeune@gmail.com>
2023-05-27 19:43:26 +0200

    [feat] added initial version of evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -3,6 +3,44 @@ from pyannote.metrics.diarization import DiarizationErrorRate
 class Evaluation:
     
     def __init__(self) -> None:
-        metric = DiarizationErrorRate()
-        # TODO: add evaluation / diarization error rate??
-        pass
+        self.der_metric = DiarizationErrorRate()
+    
+    def evaluate(self, reference, hypothesis): # Change this to file names
+        '''
+        Computes the Diarization Error Rate.
+        
+        Args:
+            reference: the ground truth
+            hypothesis: the predicted labels
+        Returns:
+            int: diarization error rate
+        '''
+        return self.der_metric(reference, hypothesis)
+    
+    def accumulated(self):
+        """
+        Computes the accumulated value across all results.
+        
+        Returns:
+            int: accumulated value
+        """
+        return abs(self.der_metric)
+    
+    def report(self):
+        """
+        Provides a summary of all files evaluated.
+        
+        Returns:
+            <type?>: summary of all results
+        """
+        return self.der_metric.report(display=True)
+        
+    def visualize(self, output_path):
+        """
+        Creates and saves a heatmap of the results.
+        
+        Args:
+            output_path: location where the graph will be saved
+        """
+        pass    
+    
