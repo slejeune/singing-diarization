26d5bffb46804523b6902707f0389c7bd4740767
Suzan Lejeune <suzan.lejeune@gmail.com>
2023-05-27 19:14:05 +0200

    [fix] optimized folder diarization

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from src import *
 
 import time
+from os import listdir
+from os.path import isfile, join
 from beepy import beep
 
 def main():
     with open('access_token.txt') as f:
         access_token = f.readlines()[0]
         
-    input_path = "audio/"
+    input_path = "audio/test.mp3"
     
     diarization = Diarization(access_token)
     evaluation = Evaluation()
 
     tic = time.perf_counter()
-    diarization.run(input_path)
+    if "." in input_path:   # Single file
+        diarization.run(input_path)
+    else:                   # Folder
+        print("Warning: you are analyzing a folder! This may take a while.")
+        for input_file in [f for f in listdir(input_path) if isfile(join(input_path, f))]:
+             # Ignore hidden files
+            if(input_file.split("/")[-1].split(".")[0] == ""):
+                continue
+            diarization.run(input_path+input_file)
     toc = time.perf_counter()
     print(f"Runtime diarization: {toc - tic:0.2f} seconds")
     
--- src/diarization.py
+++ src/diarization.py
@@ -25,51 +25,23 @@ class Diarization:
             input_path: location of the input file or folder
         '''
         
-        # Create device for running on GPU
+        # Create a pipeline running on GPU
         device = torch.device('mps')
-
         pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1", use_auth_token=self.access_token)
         pipeline.to(device)
+            
+        # Apply the pipeline to an audio file
+        diarization = pipeline(input_path)
+
+        file_name = input_path.split("/")[-1].split(".")[0]
+        output_path = "output/"+file_name+".rttm"
         
-        if "." in input_path: # A single file
+        # Dump the diarization output to disk using RTTM format
+        with open(output_path, "w") as rttm:
+            diarization.write_rttm(rttm)
             
-            # Apply the pipeline to an audio file
-            diarization = pipeline(input_path)
-
-            file_name = input_path.split("/")[-1].split(".")[0]
-            output_path = "output/"+file_name+".rttm"
-            
-            # Dump the diarization output to disk using RTTM format
-            with open(output_path, "w") as rttm:
-                diarization.write_rttm(rttm)
-                
-            reference = load_rttm(output_path)[file_name]            
-            fig, ax = plt.subplots()
-            notebook.plot_annotation(reference, ax=ax, time=True, legend=True)
-            ax.set_title(file_name, pad=20)
-            fig.savefig('plots/'+file_name+'.png')
-            
-        else: # A folder
-            print("Warning: you are (probably) analyzing multiple files in a folder! This may take a while.")
-            
-            for file in os.listdir(input_path):
-                
-                file_name = file.split("/")[-1].split(".")[0]
-                output_path = "output/"+file_name+".rttm"
-                
-                # Ignore hidden files
-                if(file_name == ""):
-                    continue
-                
-                # Apply the pipeline to an audio file
-                diarization = pipeline(input_path+file)
-
-                # Dump the diarization output to disk using RTTM format
-                with open(output_path, "w") as rttm:
-                    diarization.write_rttm(rttm)
-
-                reference = load_rttm(output_path)[file_name]            
-                fig, ax = plt.subplots()
-                notebook.plot_annotation(reference, ax=ax, time=True, legend=True)
-                ax.set_title(file_name, pad=20)
-                fig.savefig('plots/'+file_name+'.png')
+        reference = load_rttm(output_path)[file_name]            
+        fig, ax = plt.subplots()
+        notebook.plot_annotation(reference, ax=ax, time=True, legend=True)
+        ax.set_title(file_name, pad=20)
+        fig.savefig('plots/'+file_name+'.png')
