b453449120a66325f567486033e9932743d0ca43
Suzan Lejeune <suzan.lejeune@gmail.com>
2023-06-07 18:34:00 +0200

    [feat] added bar plot function

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,6 +1,9 @@
 from pyannote.metrics.diarization import DiarizationErrorRate
 from pyannote.database.util import load_rttm
 
+import matplotlib.pyplot as plt
+import pandas
+
 class Evaluation:
     
     def __init__(self) -> None:
@@ -36,16 +39,36 @@ class Evaluation:
         Provides a summary of all files evaluated.
         
         Returns:
-            <type?>: summary of all results
+            pandas.core.frame.DataFrame: summary of all results
         """
         return self.der_metric.report(display=True)
         
-    def visualize_heatmap(self, output_path):
+    def plot(self, report:pandas.core.frame.DataFrame):
         """
-        Creates and saves a heatmap of the results.
+        Creates and saves a graph of the results.
         
         Args:
+            report: dataframe of the evaluation
             output_path: location where the graph will be saved
         """
-        pass    
-    
+        
+        fig, ax = plt.subplots(1, 2, figsize=(10,8))
+        
+        # Speaking
+        speaking_df = report[:int((report.shape[0]-1)/2)]
+        speaking_df.sort_index(inplace=True)
+        ax[0].bar(speaking_df.index, speaking_df.iloc[:, 0].tolist(), color="cornflowerblue")
+        ax[0].set_title("Speaking")
+        ax[0].tick_params(labelrotation=90)
+        ax[0].set_ylabel("DER")
+        
+        # Singing
+        singing_df = report[int((report.shape[0]-1)/2):-1]
+        singing_df.sort_index(inplace=True)
+        ax[1].bar(singing_df.index, singing_df.iloc[:, 0].tolist(), color="goldenrod")
+        ax[1].set_title("Singing")
+        ax[1].tick_params(labelrotation=90)
+        ax[1].set_ylabel("DER")
+        
+        plt.tight_layout()
+        plt.show()
